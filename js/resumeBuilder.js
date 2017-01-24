cc1323e8fbae9d446ef362044edccb18a2090112
sunrise2075 <sunrise2075@163.com>
2017-01-25 00:36:32 +0800

    ready for subnission

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -24,7 +24,7 @@ var bio = {
 	"contacts": {
 		"mobile": "+8613538257831",
 		"email": "sunrise2075@163.com",
-		"github": "tigercao19860124",
+		"github": "sunrise2075",
 		"twitter": "@sunrise2075",
 		"location": "Shenzhen, Guangdong, PRC"
 	},
@@ -89,10 +89,8 @@ if(bio.skills.length>0){
 	$('#skills').append(formattedSkills);
 }
 
-var work = {};
-work.position = "Java Web Developer";
-work.employer = "Lanqiao IT school";
-work.years = 0.3;
+
+
 
 var education = {
 	"schools":[
@@ -112,24 +110,104 @@ var education = {
 	"onlineCourses": []
 };
 
-var project = {
+displayWork();
+
+var projects = {
 	"projects":[{
-		"title": "",
-		"dates": "",
-		"desciption": "",
+		"title": "Title 1",
+		"dates": "Dates 1",
+		"desciption": "Desciption 1",
+		"images": [],
+		"url": ""
+	},
+	{
+		"title": "Title 2",
+		"dates": "Dates 2",
+		"desciption": "Desciption 2",
 		"images": [],
 		"url": ""
 	}]
 }
 
+projects.display = function(){
+	for(project in projects.projects){
+		$("#projects").append(HTMLprojectStart);
+		
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		$(".project-entry:last").append(formattedTitle);
+		
+		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		$(".project-entry:last").append(formattedDates);
+		
+		var formattedDesc = HTMLprojectDescription.replace("%data%", projects.projects[project].desciption);
+		$(".project-entry:last").append(formattedDesc);
+		
+		if(projects.projects[project].images.length > 0){
+			for(image in projects.projects[project].images){
+				var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+				$(".project-entry:last").append(formattedImage);
+			}
+		}
+	}
+}
+
+projects.display();
 
 
+function displayWork(){
+	var work = {
+	"jobs":[{
+		title : "Java Web Developer",
+		employer : "Lanqiao IT school",
+		years : 0.3	,
+		dates: 'August 2016 - December 2016',
+		description: 'the description content 1......'
+	},{
+		title : "Java Web Developer",
+		employer : "Shenzhen Coordinates Healthcare Information Technology Compony",
+		years : 1.3	,
+		dates: 'Feburary 2014 - August 2016',
+		description: 'the description content 2......'
+	}]
+};
+
+for(var job in work.jobs){
+	$('#workExperience').append(HTMLworkStart);
+	var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace('%data%',work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+	
+	$('.work-entry:last').append(formattedEmployerTitle);
+	
+	var formattedDates = HTMLworkDates.replace('%data%', work.jobs[job].dates);
+	$('.work-entry:last').append(formattedDates);
+	var formattedDesc = HTMLworkDescription.replace('%data%', work.jobs[job].description);
+	$('.work-entry:last').append(formattedDesc);;
+}
+}
 
 
+$(document).click(function(loc){
+	var x = loc.pageX;
+	var y = loc.pageY;
+	console.log(x, y);
+});
+
+function inName(name){
+	name = name.trim().split(" ");
+	console.log(name);
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice[1].toLowerCase();
+	
+	return name[0] + " " + name[1];
+}
+
+//$('#main').append(internationalizeButton);
+
+$("#mapDiv").append(googleMap);
 
 
-
-
+initializeMap();
 
 
 
